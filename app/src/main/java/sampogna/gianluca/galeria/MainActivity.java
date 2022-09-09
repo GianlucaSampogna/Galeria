a48718db9233954894176775d8d8cebaef5023fa
20201tiimi0063 <gianlucascalzisampogna@gmail.com>
2022-09-09 12:04:54 -0300

    aula 09/09 Passo 10- não fiz nada ainda

--- app/src/main/java/sampogna/gianluca/galeria/MainActivity.java
+++ app/src/main/java/sampogna/gianluca/galeria/MainActivity.java
@@ -3,23 +3,47 @@ package sampogna.gianluca.galeria;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    List<String> photos = new ArrayList<>();
+    MainAdapter mainAdapter;
 
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.tbMain);
         setSupportActionBar(toolbar);
 
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File[] files = dir.listFiles();
+        for(int i = 0; i < files.length; i++) {
+            photos.add(files[i].getAbsolutePath());
+        }
 
+        mainAdapter = new MainAdapter(MainActivity.this, photos);
+
+        RecyclerView rvGallery = findViewById(R.id.rvGallery);
+        rvGallery.setAdapter(mainAdapter);
+
+        float w = getResources().getDimension(R.dimen.itemWidth);
+        int numberOfColumns = Utils.calculateNoOfColumns(MainActivity.this, w);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, numberOfColumns);
+        rvGallery.setLayoutManager(gridLayoutManager);
 
     }
 
@@ -42,9 +66,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
-
-}
+    }
 
 
---
+++ app/src/main/java/sampogna/gianluca/galeria/MeuViewHolder.java
@@ -0,0 +1,12 @@
+package sampogna.gianluca.galeria;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MeuViewHolder extends RecyclerView.ViewHolder {
+    public MeuViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
